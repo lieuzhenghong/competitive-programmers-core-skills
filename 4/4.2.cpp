74241e0bb6dd686208ee75a0265fa8730dbf5a93
rajats98 <33171120+rajats98@users.noreply.github.com>
2019-10-14 09:20:52 +0530

    Added working solution for 4.2 (Maximal Distance)
    We only have to track indices of minimum and maximum value after each insertion

--- 4/4.2.cpp
+++ 4/4.2.cpp
@@ -1,6 +1,25 @@
-/* Max Manhattan Distance */
+#include<bits/stdc++.h>
+using namespace std;
 
-int main()
-{
-    return 0;
+int main(){
+	int n;
+	cin>>n;
+	
+	int mini = INT_MAX;
+	int maxi = INT_MIN;
+	int minindex = 0;
+	int maxindex = 0;
+	for(int i=1;i<=n;i++){
+		int a;
+		cin>>a;
+		if(mini>a){
+			mini = a;
+			minindex = i;
+		}
+		if(maxi<a){
+			maxi = a;
+			maxindex = i;
+		}
+		cout<<min(minindex,maxindex)<<" "<<max(minindex,maxindex)<<endl;
+	}	
 }
